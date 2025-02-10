13cdf26a2465773d0a57937e1f61f3991ed6d23d
Jude Adeyemi <supremepraiz2@gmail.com>
2025-02-10 13:27:49 +0100

    my second commit

--- number_classification/settings.py
+++ number_classification/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7xmwz(+mpw5@srb%ib5ud&hontnr-5h4f)z$wb35_7+9^+5^m0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
